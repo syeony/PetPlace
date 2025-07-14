219d98f597a20fea420407f55afc2108f09f6056
DESKTOP-DLOPF19\SSAFY <dw8624@naver.com>
2025-07-14 22:51:24 +0900

    fix: MailService.java 버전 오류 수정

--- backend_skeleton/core-module/src/main/java/com/ssafy/core/service/MailService.java
+++ backend_skeleton/core-module/src/main/java/com/ssafy/core/service/MailService.java
@@ -14,8 +14,8 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
