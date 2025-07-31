4fc8d11b820a147f517ea63262611a4d6f13466b
이도형 <eehgud99@gmail.com>
2025-07-31 13:22:46 +0900

    Feat: API연동을 제외한 화면구현 완료

--- Front/PetPlace/app/src/main/java/com/example/petplace/presentation/feature/feed/CommentBottomSheet.kt
+++ Front/PetPlace/app/src/main/java/com/example/petplace/presentation/feature/feed/CommentBottomSheet.kt
@@ -1,5 +1,7 @@
 package com.example.petplace.presentation.feature.feed
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,31 +10,38 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.platform.LocalFocusManager // 키보드 포커스 제어
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +49,7 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.petplace.R
 import com.example.petplace.presentation.feature.feed.model.Comment
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CommentBottomSheet(
@@ -50,7 +60,12 @@ fun CommentBottomSheet(
         skipPartiallyExpanded = true, // 부분 확장을 건너뜀
         confirmValueChange = { true },
     )
-
+    //채팅 펼치기
+    val expandedStates = remember { mutableStateListOf<Boolean>().apply {
+        repeat(comments.size) { add(false) }
+    } }
+    var commentText by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
     LaunchedEffect(Unit) {
         sheetState.show() // 강제로 Expanded 상태로
     }
@@ -61,23 +76,73 @@ fun CommentBottomSheet(
         containerColor = Color.White,
         shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)
     ) {
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(2f / 3f) // 화면의 2/3만 차지하도록 설정
-            .padding(16.dp)) {
-            Text(
-                text = "댓글",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            LazyColumn {
-                items(comments) { comment ->
-                    if(comment.replies.isEmpty()){
+        Scaffold(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.75f) // 화면 75% 정도 차지
+                .navigationBarsPadding(), // 하단 시스템바까지 패딩
+
+            bottomBar = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    TextField(
+                        value = commentText,
+                        onValueChange = { commentText = it },
+                        modifier = Modifier.weight(1f),
+                        placeholder = { Text("댓글을 입력하세요") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Send
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onSend = {
+                                if (commentText.isNotBlank()) {
+                                    // 전송 로직
+                                    println("전송: $commentText")
+
+                                    // 입력 초기화
+                                    commentText = ""
+
+                                    // 키보드 내리기
+                                    focusManager.clearFocus()
+                                }
+                            }
+                        ),
+                        singleLine = true
+                    )
+//                    Spacer(modifier = Modifier.width(8.dp))
+//                    Button(onClick = { /* 전송 */ }) {
+//                        Text("전송")
+//                    }
+                }
+            }
+        ){innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "댓글",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                LazyColumn(
+//                verticalArrangement = Arrangement.spacedBy(12.dp) // 아이템 간격 12dp
+                ) {
+                    items(comments.size) { index ->
+                        val comment = comments[index]
+
                         Column(modifier = Modifier.padding(vertical = 8.dp)) {
-                            Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
                                 Image(
                                     painter = rememberAsyncImagePainter(comment.profileImage),
                                     contentDescription = null,
@@ -85,7 +150,7 @@ fun CommentBottomSheet(
                                 )
                                 Spacer(modifier = Modifier.width(8.dp))
                                 Column {
-                                    Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
+                                    Row(verticalAlignment = Alignment.CenterVertically) {
                                         Text(text = comment.author, fontWeight = FontWeight.Bold)
                                         Spacer(modifier = Modifier.width(6.dp))
                                         Text(text = comment.town, fontSize = 12.sp, color = Color.Gray)
@@ -93,106 +158,77 @@ fun CommentBottomSheet(
                                     Text(text = comment.text)
                                 }
                                 Spacer(modifier = Modifier.weight(1f))
-//
                             }
-                            if (comment.replies.isEmpty()) {
-                                TextButton(onClick = { /* 답글 */ }) {
+
+                            // 답글 버튼
+                            if(comment.replies.isEmpty()){
+                                TextButton(
+                                    onClick = {
+
+                                    }
+                                ) {
                                     Text("답글 달기", fontSize = 10.sp)
                                 }
-                            } else {
-                                comment.replies.forEach { reply ->
-                                    Row(modifier = Modifier.padding(start = 36.dp)) {
-                                        Text("${reply.author}: ${reply.text}")
+                            }
+                            else{
+                                TextButton(
+                                    onClick = {
+                                        expandedStates[index] = !expandedStates[index]
                                     }
+                                ) {
+                                    Text("댓글 보기", fontSize = 10.sp)
                                 }
                             }
-                        }
-                    }
-                    else{
-                        Column(modifier = Modifier.padding(vertical = 8.dp)) {
-                            Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
-                                Image(
-                                    painter = rememberAsyncImagePainter(comment.profileImage),
-                                    contentDescription = null,
-                                    modifier = Modifier.size(35.dp).clip(CircleShape)
-                                )
-                                Spacer(modifier = Modifier.width(8.dp))
+                            // 대댓글 표시 (토글 상태일 때만)
+                            if (expandedStates[index]) {
                                 Column {
-                                    Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
-                                        Text(text = comment.author, fontWeight = FontWeight.Bold)
-                                        Spacer(modifier = Modifier.width(6.dp))
-                                        Text(text = comment.town, fontSize = 12.sp, color = Color.Gray)
-                                    }
-                                    Text(text = comment.text)
-                                }
-                                Spacer(modifier = Modifier.weight(1f))
-                                if (comment.isMine) {
-                                    IconButton(onClick = { /* 수정 */ }) {
-                                        Icon(Icons.Default.Create, contentDescription = "수정")
-                                    }
-                                    IconButton(onClick = { /* 삭제 */ }) {
-                                        Icon(Icons.Default.Clear, contentDescription = "삭제")
-                                    }
-                                }
-                            }
-                            if (comment.replies.isEmpty()) {
-                                TextButton(onClick = { /* 답글 */ }) {
-                                    Text("답글 달기", fontSize = 10.sp)
-                                }
-                            } else {
-                                comment.replies.forEach { reply ->
-                                    Row(
-                                        verticalAlignment = Alignment.CenterVertically
-                                    ) {
-                                        Spacer(modifier = Modifier.width(12.dp))
-                                        // 아이콘
-                                        Icon(
-                                            painter = painterResource(R.drawable.ic_reply),
-                                            contentDescription = null,
-                                            modifier = Modifier
-                                                .size(20.dp)     // 아이콘 크기만큼만
-//                                                .padding(), // 텍스트와 간격
-                                            ,
-                                            tint = Color.Gray
-                                        )
-                                        Spacer(modifier = Modifier.width(24.dp))
-                                        Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
+                                    comment.replies.forEach { reply ->
+                                        Row(
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            modifier = Modifier.padding(start = 36.dp, top = 4.dp, bottom = 4.dp)
+                                        ) {
+                                            Icon(
+                                                painter = painterResource(R.drawable.ic_reply),
+                                                contentDescription = null,
+                                                modifier = Modifier.size(20.dp),
+                                                tint = Color.Gray
+                                            )
+                                            Spacer(modifier = Modifier.width(8.dp))
                                             Image(
                                                 painter = rememberAsyncImagePainter(reply.profileImage),
                                                 contentDescription = null,
-                                                modifier = Modifier.size(35.dp).clip(CircleShape)
+                                                modifier = Modifier.size(30.dp).clip(CircleShape)
                                             )
                                             Spacer(modifier = Modifier.width(8.dp))
                                             Column {
-                                                Row(verticalAlignment = androidx.compose.ui.Alignment.CenterVertically) {
+                                                Row(verticalAlignment = Alignment.CenterVertically) {
                                                     Text(text = reply.author, fontWeight = FontWeight.Bold)
                                                     Spacer(modifier = Modifier.width(6.dp))
                                                     Text(text = reply.town, fontSize = 12.sp, color = Color.Gray)
                                                 }
                                                 Text(text = reply.text)
                                             }
-                                            Spacer(modifier = Modifier.weight(1f))
-//                                            if (reply.isMine) {
-//                                                IconButton(onClick = { /* 수정 */ }) {
-//                                                    Icon(Icons.Default.Create, contentDescription = "수정")
-//                                                }
-//                                                IconButton(onClick = { /* 삭제 */ }) {
-//                                                    Icon(Icons.Default.Clear, contentDescription = "삭제")
-//                                                }
-//                                            }
+
                                         }
-
                                     }
-
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalArrangement = Arrangement.End
+                                    ) {
+                                        TextButton(
+                                            onClick = {  }
+                                        ) {
+                                            Text("답글 달기", fontSize = 10.sp)
+                                        }
+                                    }
                                 }
                             }
                         }
-
-
                     }
-
                 }
+
             }
         }
+
     }
 }
