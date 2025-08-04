f94f52c12cfe9c5e099e881fb95a4a74ec5de06b
kim0904min <kim0904min@gmail.com>
2025-08-04 13:23:59 +0900

    Feat: Image 서비스 인터페이스 및 구현체 추가

---
+++ Back/petPlace/src/main/java/com/minjeok4go/petplace/image/service/ImageService.java
@@ -0,0 +1,13 @@
+package com.minjeok4go.petplace.image.service;
+
+import com.minjeok4go.petplace.common.constant.ImageType;
+import com.minjeok4go.petplace.image.dto.ImageRequest;
+import com.minjeok4go.petplace.image.dto.ImageResponse;
+
+import java.util.List;
+
+public interface ImageService {
+    ImageResponse upload(ImageRequest req);
+
+    List<ImageResponse> getImages(ImageType refType, Long refId);
+}
