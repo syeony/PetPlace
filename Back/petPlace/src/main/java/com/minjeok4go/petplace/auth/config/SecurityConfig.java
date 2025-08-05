24814d68368270468b57c16c2d5e4ffec4da5294
정유진 <dw8624@naver.com>
2025-08-06 00:02:28 +0900

    Fix: SignUp Request Dto 필수만 보이게 수정

--- Back/petPlace/src/main/java/com/minjeok4go/petplace/auth/config/SecurityConfig.java
+++ Back/petPlace/src/main/java/com/minjeok4go/petplace/auth/config/SecurityConfig.java
@@ -40,8 +40,7 @@ public class SecurityConfig {
                                 "/api/user/check-username",
                                 "/api/user/check-nickname",
                                 "/api/user/certifications/prepare",  // 본인인증 준비 API (DEPRECATED)
-                                "/api/user/certifications/otp/request",  // 본인인증 OTP 요청
-                                "/api/user/certifications/otp/confirm",  // 본인인증 OTP 확인
+
                                 // 인증 API
                                 "/api/auth/login",
                                 "/api/auth/refresh",
