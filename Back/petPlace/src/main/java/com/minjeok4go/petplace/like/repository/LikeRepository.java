6bf65875d47f5946b088b3ecdb4336b6d4490bca
kim0904min <kim0904min@gmail.com>
2025-08-06 21:42:39 +0900

    Feat: Likes DB를 조회하기 위한 레포지토리 추가

---
+++ Back/petPlace/src/main/java/com/minjeok4go/petplace/like/repository/LikeRepository.java
@@ -0,0 +1,11 @@
+package com.minjeok4go.petplace.like.repository;
+
+import com.minjeok4go.petplace.feed.entity.Feed;
+import com.minjeok4go.petplace.like.entity.Likes;
+import com.minjeok4go.petplace.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Likes, Long> {
+
+    Likes findByFeedAndUser(Feed feed, User user);
+}
