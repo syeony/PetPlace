990a7b046d7bc489a0c8332cc3e5bf05272ae181
kim0904min <kim0904min@gmail.com>
2025-08-12 13:57:15 +0900

    Fix: 리펙토링에 따른 레포지토리 메소드 Query문 추가

--- Back/petPlace/src/main/java/com/minjeok4go/petplace/feed/repository/FeedTagRepository.java
+++ Back/petPlace/src/main/java/com/minjeok4go/petplace/feed/repository/FeedTagRepository.java
@@ -2,6 +2,7 @@ package com.minjeok4go.petplace.feed.repository;
 
 import com.minjeok4go.petplace.feed.entity.FeedTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface FeedTagRepository extends JpaRepository<FeedTag, Long> {
     List<FeedTag> findByFeedId(Long feedId);
     List<FeedTag> findByTagId(Long tagId);
 
+    @Query("select ft.tag.id from FeedTag ft where ft.feed.id = :feedId")
     List<Long> findTagIdByFeedId(Long feedId);
 }
