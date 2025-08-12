cdfe2c3a6b6c6584223ebbc639fb1bf364aff25f
kim0904min <kim0904min@gmail.com>
2025-08-12 16:53:49 +0900

    Feat: Image 삭제용 레포지토리 메소드 추가

--- Back/petPlace/src/main/java/com/minjeok4go/petplace/image/repository/ImageRepository.java
+++ Back/petPlace/src/main/java/com/minjeok4go/petplace/image/repository/ImageRepository.java
@@ -3,6 +3,9 @@ package com.minjeok4go.petplace.image.repository;
 import com.minjeok4go.petplace.common.constant.ImageType;
 import com.minjeok4go.petplace.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +13,8 @@ import java.util.Optional;
 public interface ImageRepository extends JpaRepository<Image, Long> {
     List<Image> findByRefTypeAndRefIdOrderBySortAsc(ImageType refType, Long refId);
     Optional<Image> findByRefTypeAndRefIdAndSort(ImageType refType, Long refId, Integer sort);
+
+    @Modifying
+    @Query("delete from Image i where i.refType = :type and i.refId = :refId")
+    void deleteAllByRef(@Param("type") ImageType type, @Param("refId") Long refId);
 }
