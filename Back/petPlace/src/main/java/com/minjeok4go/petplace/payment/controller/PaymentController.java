c7116da3232d04949ae882b6b2bec4ca3912ecaf
조경호 <ghwh323232@naver.com>
2025-08-13 15:15:45 +0900

    Refactor: 결제시스템 배포용 수정

--- Back/petPlace/src/main/java/com/minjeok4go/petplace/payment/controller/PaymentController.java
+++ Back/petPlace/src/main/java/com/minjeok4go/petplace/payment/controller/PaymentController.java
@@ -22,6 +22,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/payments")
 @RequiredArgsConstructor
